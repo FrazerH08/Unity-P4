92e9522705513bbdaca3415bc1f047a51905243a
Frazer Harness <654994@eastridingcollege.ac.uk>
2025-03-06 22:14:05 +0000

    Level,2,3,4

--- UnityLearning/Assets/Scripts/ThirdPersonCam.cs
+++ UnityLearning/Assets/Scripts/ThirdPersonCam.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class ThirdPersonCam : MonoBehaviour
 {
-    [Header("Refrences")]
+    [Header("References")]
     public Transform orientation;   
     public Transform player;
     public Transform playerObj;
     public Rigidbody rb;
 
     public float rotationSpeed;
+
+    public Transform combatLookAt;
+    public GameObject thirdPersonCam;
+    public GameObject combatCam;
+    public GameObject topDownCam;
+
+    public CameraStyle currentStyle;
+
+    public enum CameraStyle
+    {
+        Basic,
+        Combat,
+        Topdown
+    }
 
     private void Start()
     {
@@ -21,15 +35,44 @@ public class ThirdPersonCam : MonoBehaviour
 
     private void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) SwitchCameraStyle(CameraStyle.Basic);
+        if (Input.GetKeyDown(KeyCode.Alpha2)) SwitchCameraStyle(CameraStyle.Combat);
+        if (Input.GetKeyDown(KeyCode.Alpha3)) SwitchCameraStyle(CameraStyle.Topdown);
+
         Vector3 viewDir = player.position -  new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward =viewDir.normalized;
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 inputdir = orientation.forward * verticalInput + orientation.right *horizontalInput;
+        if(currentStyle == CameraStyle.Basic  || currentStyle == CameraStyle.Topdown)
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 inputDir = orientation.forward * verticalInput + orientation.right *horizontalInput;
 
-        if(inputdir != Vector3.zero){
-            playerObj.forward = Vector3.Slerp(playerObj.forward, inputdir.normalized , Time.deltaTime * rotationSpeed);
+            if(inputDir != Vector3.zero)
+                playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized , Time.deltaTime * rotationSpeed);
         }
+
+        else if(currentStyle == CameraStyle.Combat)
+        {
+            Vector3 dirToCombatLookAt = combatLookAt.position - new Vector3(transform.position.x, combatLookAt.position.y, transform.position.z);
+            orientation.forward = dirToCombatLookAt.normalized;
+
+            playerObj.forward = dirToCombatLookAt.normalized;
+        }
+    }
+
+    private void SwitchCameraStyle(CameraStyle newStyle) 
+    {
+        combatCam.SetActive(false);
+        thirdPersonCam.SetActive(false);
+        topDownCam.SetActive(false);
+
+        if (newStyle == CameraStyle.Basic) thirdPersonCam.SetActive(true);
+        if (newStyle == CameraStyle.Combat) combatCam.SetActive(true);
+        if (newStyle == CameraStyle.Topdown) topDownCam.SetActive(true);
+
+        currentStyle = newStyle;
+
     }
 }
